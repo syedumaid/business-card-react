7f93a519f0352fcead5e6239bbdc64b63e9c9b22
syedumaid <umaid12@gmail.com>
2022-12-06 11:42:40 +0530

    Initial Commit

---
+++ components/About.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function About() {
+    return(
+        <div className="text-section">
+            <h4>About</h4>
+            <p>
+                I started Amazon from my Mother's Garage and now I am the 2nd richest person in the world.
+                I am skilled in Business Strategy Marketing, Goods Transfering and above all, running the world's biggest business.
+            </p>
+            
+            <h4>Interests</h4>
+            <p>
+                Going to Space. Being the #1. Music. Attending Comedy shows.
+            </p>
+        </div>
+    )
+}
